546ec40a6e2cc5db07628b0879fbecb84f7d6f60
Marcos Ricardo <marc_ric@hotmail.com>
2010-12-11 22:16:36 -0200

    MROS commit 001

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoodleLms::Application.config.secret_token = '3aa08c5b419fef3329661053e68a1e8b61a81589fd1dcd16b30e093d2c081fcc34957b9bb80cbe103fc296ad1c22d2bd1f13c478db3d3a6b81d19e14bdd3aa1c'
