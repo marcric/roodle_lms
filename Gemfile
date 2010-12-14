55211d3826707cbc81161795b6e3907f16dd68b4
Victor Goff <keeperotphones@gmail.com>
2010-12-14 01:30:04 -0600

    Added spec for controller and user continues gh-17 and gh-18

--- Gemfile
+++ Gemfile
@@ -11,7 +11,9 @@ gem 'devise', '1.1.5'
 # and rake tasks are available in development mode:
 
 group :development, :test do
+  gem 'rspec'
   gem 'rspec-rails', ' >= 2.0.0.beta.22'
+  gem 'cucumber'
   gem 'cucumber-rails'
   gem 'spork'
   gem 'webrat'
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,26 @@
+describe User do
+    context "when an user is created it has to have an e-mail address" do
+      before(:each) do
+        @user = User.create! :email => "test@test.com", :password => '123abc'
+        @user2 = User.create "Test2"
+      end 
+      
+      it "validates the user attributes" do
+        @user.should be_valid
+      end
+
+      it "doesn't validates the user attributes" do
+        @user2.should_not be_valid
+      end
+    end
+
+    context "when an user is searched by its e-mail address" do 
+      before(:each) do
+        @user = User.create! :email => "test@test.com", :password => '123abc'
+      end
+
+      it "should find the user" do
+        User.find_by_email("test@test.com").should be_true
+      end
+    end
+end
