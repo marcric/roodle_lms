762c19a59489a9d043d724e21f976bd2c62a49be
Victor Goff <keeperotphones@gmail.com>
2010-12-24 17:13:25 -0600

    Added specifications derived from group discussion.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,28 +1,54 @@
 require 'spec_helper'
 
 describe User do
-    context "when an user is created it has to have an e-mail address" do
-      before(:each) do
-        @user = User.create! :email => "test@test.com", :password => '123abc'
-        @user2 = User.create "Test2"
-      end 
-      
-      it "validates the user attributes" do
-        @user.should be_valid
-      end
+  before(:each) do
+    @user = User.create! :email => "test@test.com", :password => '123abc'
+    @user2 = User.create "Test2"
+  end 
+  context "when an user is created it has to have an e-mail address" do
 
-      it "doesn't validates the user attributes" do
-        @user2.should_not be_valid
-      end
+    it "validates the user attributes" do
+      @user.should be_valid
     end
 
-    context "when an user is searched by its e-mail address" do 
-      before(:each) do
-        @user = User.create! :email => "test@test.com", :password => '123abc'
+    it "should validate the user attributes" do
+      @user2.should_not be_valid
+    end
+  end
+
+  context "when an user is searched by its e-mail address" do 
+
+    it "should find the user" do
+      User.find_by_email("test@test.com").should be_true
+    end
+  end
+
+  [:student, :teacher, :mentor, :patron].each do |role|
+    context "As a #{role} in the Student, Teacher, Mentor and Patron roles, I " do
+
+      [:user_name, :city, :country, :tz, :avatar].each do |attribute|
+        it "should see #{attribute} from my colleagues: " do
+          pending
+        end
       end
 
-      it "should find the user" do
-        User.find_by_email("test@test.com").should be_true
+      [:full_name, :email, :address].each do |attribute|
+        it "should not see #{attribute} from my colleagues: " do
+          pending
+        end
       end
+
+      context "As a logged user in the Admin role, I should see from other users: " do
+        [:full_name, :email, :address].each do |attribute|
+          it "should see #{attribute} " do
+            pending
+          end
+        end
+      end
+
     end
+  end
+
 end
+
+
